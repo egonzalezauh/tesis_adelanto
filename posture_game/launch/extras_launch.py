7c844eb07b2749943695e97a63d4d7bddb22d2b6
egonzalezauh <erickgonzalez0213@gmail.com>
2025-07-21 10:45:29 -0500

    rutas dinamicas a los launch

--- posture_game/launch/extras_launch.py
+++ posture_game/launch/extras_launch.py
@@ -1,5 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
     return LaunchDescription([
@@ -29,7 +31,10 @@ def generate_launch_description():
             package='usb_cam',
             executable='usb_cam_node_exe',
             name='usb_cam',
-            parameters=['/home/erickgonza/ros2_tesis/posture_game/config/usb_cam_params.yaml'],
+            parameters=[os.path.join(
+                get_package_share_directory('posture_game'),
+                'config',
+                'usb_cam_params.yaml')],
             output='log'
         ),
  
